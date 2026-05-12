b781fa2fffe5312ae6b0b05a3d904d856e3fcfae
vyze <92610500+vyzzze@users.noreply.github.com>
2026-05-12 11:06:04 +0200

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   vite: {
-    base: "/lume-web/",
+    base: "./",
     build: {
       outDir: "dist",
     },
